dcad13c68e5e80236b713744505db7736adc374c
GIORGIO <giorgiobridarolli@gmail.com>
2026-08-25 09:48:31 +0200

    Set up PIC command and data registers
    Initialize PIC commands and data for remapping.

--- porte.asm
+++ porte.asm
@@ -1,3 +1,7 @@
+mov PIC1_COMMAND, 0x20
+mov PIC1_DATA, 0x21
+mov PCI2_COMMAND, 0xA0
+mov PCI2_DATA, 0xA1
 global outb
 outb:
     mov dx, word [esp + 4]
@@ -8,4 +12,17 @@ global inb
 inb:
     mov dx, word [esp+4]
     in al, dx
+    ret
+global pic_remap
+pic_remap:
+    outb(PCI1_COMMAND, 0x11)
+    outb(PCI2_COMMAND, 0x11)
+    outb(PCI1_DATA, 0x20)
+    outb(PCI2_DATA, 0x28)
+    outb(PCI1_DATA, 0x04)
+    outb(PCI2_DATA, 0x02)
+    outb(PCI1_DATA, 0x01)
+    outb(PCI2_DATA, 0x01)
+    outb(PCI1_DATA, 0xFC)
+    outb(PCI2_DATA, 0xFF)
     ret
