dbb1e0dad366fe65118627745fb58bed9f5375b1
GIORGIO <giorgiobridarolli@gmail.com>
2026-08-28 10:17:10 +0200

    Implement basic kernel main function and video memory

---
+++ kernel.cpp
@@ -0,0 +1,14 @@
+typedef unsigned char uint8_t;
+typedef unsigned short uint16_t;
+typedef unsigned int uint32_t;
+
+extern "C" void outb(unsigned short porta, unsigned char valore);
+extern "C" unsigned char inb(unsigned short porta);
+
+char* memoria_video = (char*)0xb8000;
+
+extern "C" void kernel_main() {
+    memoria_video[0] = 'K';
+    memoria_video[1] = 0x0F;
+    while (1) {}
+}
