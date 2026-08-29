0b2a830104a48571ccb1246e955cc5e448cfe45e
GIORGIO <giorgiobridarolli@gmail.com>
2026-08-29 10:04:56 +0200

    Fill video memory with 'X' characters

--- kernel.cpp
+++ kernel.cpp
@@ -10,5 +10,11 @@ char* memoria_video = (char*)0xb8000;
 extern "C" void kernel_main() {
     memoria_video[0] = 'K';
     memoria_video[1] = 0x0F;
+
+    for (int i = 0; i < 80; i++) {
+        memoria_video[i*2] = 'X';
+        memoria_video[i*2+1] = 0x0F;
+    }
+
     while (1) {}
 }
