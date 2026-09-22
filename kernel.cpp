2ea3f806e620fb70a5d8f68241e2003e5155397f
GIORGIO <giorgiobridarolli@gmail.com>
2026-09-22 15:39:15 +0200

    Comment out lettera_a definition
    Comment out the definition of 'lettera_a' in kernel.cpp.

--- kernel.cpp
+++ kernel.cpp
@@ -104,12 +104,14 @@ void disegna_rettangolo(int inizio_x, int fine_x, int inizio_y, int fine_y, unsi
         }
     }
 }
-char lettera_a = [0, 0, , 1, 1, 0, 0, 0,
+/*char lettera_a = [0, 0, , 1, 1, 0, 0, 0,
              0, 0, 1, 0, 0, 1, 0, 0,
              0, 0, 1, 0, 0, 1, 0, 0,
              0, 0, 1, 0, 0, 1, 0, 0,
              0, 0, 1, 1, 1, 1, 0, 0,
              0, 0, 1, 0, 0, 1, 0, 0];
+
+*/
 void disegna_lettera(unsigned char lettera);
 void stampa_lettera(char lettera, int colore){
     if (lettera == '\n'){
